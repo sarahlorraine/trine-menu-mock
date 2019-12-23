13b8290d1e108fcbcf63e25b856ed667a368c41d
Sarah Newton <sarahnewton@Sarahs-MacBook.lan>
2019-12-23 17:13:27 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jag älskar Daniel Pie Face
         </p>
         <a
           className="App-link"
